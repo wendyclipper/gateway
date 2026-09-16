4a5593d78ac78097faf63a817de961377d9c9be5
wendyclipper <wendydansschool@gmail.com>
2026-09-16 11:57:58 +0200

    Fix Bazaar resource URL

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const server = new x402ResourceServer(facilitator)
 app.use(
   paymentMiddleware(
     {
-      "POST /opportunities": {
+      "POST https://gateway-production-1a96.up.railway.app/opportunities": {
         accepts: [
           {
             scheme: "exact",
